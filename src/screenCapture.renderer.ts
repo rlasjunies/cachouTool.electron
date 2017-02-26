3a84d42280be84373dc98539ff998d9396a5cf64
Richard Lasjunies <rlasjunies@gmail.com>
2017-02-26 10:44:43 +0100

    + file store the screen captured

---
+++ src/screenCapture.renderer.ts
@@ -0,0 +1,32 @@
+import { ipcRenderer, desktopCapturer, screen } from "electron";
+import * as evtDef from "./eventDef";
+import * as path from "path";
+import * as fs from "fs";
+import { consoleLogMain } from "./helper.renderer";
+import * as srv from "./screenCapture.service";
+
+ipcRenderer.on(evtDef.SCREENCAPTURE_CLICKED, onCapture);
+
+export function onCapture(evt, targetPath: string) {
+    srv.getMainSource(desktopCapturer, screen, source => {
+        consoleLogMain(`[onCapture] - ${targetPath}`);
+
+        const png = source.thumbnail.toPNG();
+
+        createFolderIfNotExists(targetPath);
+        const filePath = createFileName(targetPath);
+
+        srv.writeScreenshot(png, filePath);
+    });
+}
+
+function createFolderIfNotExists(folderName: string) {
+    if (!fs.existsSync(folderName)) {
+        fs.mkdirSync(folderName);
+    }
+}
+
+function createFileName( folderName: string ) {
+    // return path.join(folderName, new Date() + ".png");
+    return path.join(folderName, new Date().getMilliseconds() + ".png");
+}
---
+++ src/screenCapture.service.ts
@@ -0,0 +1,27 @@
+import { ipcRenderer, desktopCapturer, screen } from "electron";
+import * as evtDef from "./eventDef";
+import * as path from "path";
+import * as fs from "fs";
+import { consoleLogMain } from "./helper.renderer";
+
+export function getMainSource(inDesktopCapturer: Electron.DesktopCapturer, inScreen: Electron.Screen, done: (source: Electron.DesktopCapturerSource) => void) {
+    const options: Electron.DesktopCapturerOptions = {
+        types: ["screen"],
+        thumbnailSize: inScreen.getPrimaryDisplay().workAreaSize
+    };
+
+    inDesktopCapturer.getSources(options, (err: Error, sources: Electron.DesktopCapturerSource[]) => {
+        if (err) consoleLogMain(`Cannot capture screen: ${err.message} \n ${err.stack}`);
+
+        const isMainSource = (source: Electron.DesktopCapturerSource, index, arr) => source.name === "Entire Screen" || source.name === "Screen 1";
+
+        done( sources.filter(isMainSource)[0] );
+    });
+}
+
+export function writeScreenshot(png: Buffer, filePath: string) {
+    fs.writeFile(filePath, png, err => {
+        if ( err ) consoleLogMain(`Failed to write screen: ${err.message}`);
+        consoleLogMain("screen shot taken");
+    });
+}
