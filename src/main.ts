cee5ec0937aa1f235a44b4a59cd81e467a2cf10c
Richard Lasjunies <rlasjunies@gmail.com>
2017-02-26 11:33:30 +0100

    - move tray menu mgt out of the shortcuts

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,6 @@ ipcMain.on(evtDef.MAIN_CONSOLE_LOG, (evt, message) => {
 
 function addClipboardManager() {
     const STACK_LENGTH = 10;
-    const MENU_ITEM_MAC_LENGTH = 20;
     let stack: string[] = [];
 
     checkClipboardPolling1sec(clipboard, clipboardChanged);
@@ -41,23 +40,6 @@ function addClipboardManager() {
         stack = addToStack(text, stack);
         addTrayMenu(stack);
         // registerShortcuts(globalShortcut, clipboard, stack);
-    }
-
-    function addTrayMenu(stack: string[]) {
-        const trayMenuTemplateInit: Electron.MenuItemOptions[] =
-            [
-                {
-                    type: "separator"
-                },
-                {
-                    label: "Quit",
-                    click: _ => { app.quit(); }
-                }
-            ];
-        const trayMenuTemplate = createMenuTemplate(stack).concat(trayMenuTemplateInit);
-
-        const trayMenu = Menu.buildFromTemplate(trayMenuTemplate);
-        appTray.setContextMenu(trayMenu);
     }
 
     function registerShortcuts(inGlobalShortcut: Electron.GlobalShortcut, inClipboard: Electron.Clipboard, stack: string[]) {
@@ -72,6 +54,53 @@ function addClipboardManager() {
     }
 
 
+
+    function addToStack(item: string, stack: string[]): string[] {
+        return [item].concat(stack.length >= STACK_LENGTH ? stack.slice(0, stack.length - 1) : stack);
+    }
+
+    function checkClipboardPolling1sec(clipboard: Electron.Clipboard, onChangeCallback: Function) {
+        let cache = ""; // clipboard.readText();
+
+        // timerClipboardPolling = setInterval(checkClipboard(clipboard, onChangeCallback) , 1000);
+        // timerClipboardPolling = setInterval(checkClipboard , 1000);
+        setInterval(checkClipboard, 1000);
+
+        // function checkClipboard(clipboard: Electron.Clipboard, onChangeCallback: Function) {
+        function checkClipboard() {
+            let latest = clipboard.readText();
+            if (cache !== latest) {
+                cache = latest;
+                onChangeCallback(cache);
+            }
+
+        }
+    }
+}
+
+function addTray() {
+    appTray = new Tray(__dirname + "\\..\\assets\\trayIcon.jpg");
+    appTray.setToolTip("Clipboard history");
+
+};
+
+function addTrayMenu(stack: string[]) {
+    const MENU_ITEM_MAC_LENGTH = 20;
+
+    const trayMenuTemplateInit: Electron.MenuItemOptions[] =
+        [
+            {
+                type: "separator"
+            },
+            {
+                label: "Quit",
+                click: _ => { app.quit(); }
+            }
+        ];
+    const trayMenuTemplate = createMenuTemplate(stack).concat(trayMenuTemplateInit);
+
+    const trayMenu = Menu.buildFromTemplate(trayMenuTemplate);
+    appTray.setContextMenu(trayMenu);
 
     function createMenuTemplate(stack: string[]): Electron.MenuItemOptions[] {
         const menuIfEmptyStack: Electron.MenuItemOptions[] = [
@@ -105,32 +134,4 @@ function addClipboardManager() {
     function formatMenuItem(stackItem: string): string {
         return stackItem.length >= MENU_ITEM_MAC_LENGTH ? stackItem.slice(0, MENU_ITEM_MAC_LENGTH) + "..." : stackItem;
     }
-
-    function addToStack(item: string, stack: string[]): string[] {
-        return [item].concat(stack.length >= STACK_LENGTH ? stack.slice(0, stack.length - 1) : stack);
-    }
-
-    function checkClipboardPolling1sec(clipboard: Electron.Clipboard, onChangeCallback: Function) {
-        let cache = ""; // clipboard.readText();
-
-        // timerClipboardPolling = setInterval(checkClipboard(clipboard, onChangeCallback) , 1000);
-        // timerClipboardPolling = setInterval(checkClipboard , 1000);
-        setInterval(checkClipboard, 1000);
-
-        // function checkClipboard(clipboard: Electron.Clipboard, onChangeCallback: Function) {
-        function checkClipboard() {
-            let latest = clipboard.readText();
-            if (cache !== latest) {
-                cache = latest;
-                onChangeCallback(cache);
-            }
-
-        }
-    }
 }
-
-function addTray() {
-    appTray = new Tray(__dirname + "\\..\\assets\\trayIcon.jpg");
-    appTray.setToolTip("Clipboard history");
-
-};
