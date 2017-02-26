9e206ba7b91dd5fea20fa8cd48dde841638c4d05
Richard Lasjunies <rlasjunies@gmail.com>
2017-02-26 12:28:08 +0100

    - log in the renderer and in main at the same time

--- src/share/helper.renderer.ts
+++ src/share/helper.renderer.ts
@@ -2,5 +2,6 @@ import { ipcRenderer} from "electron";
 import * as evtDef from "./eventDef";
 
 export function consoleLogMain(message: string) {
+    console.log(message);
     ipcRenderer.send(evtDef.MAIN_CONSOLE_LOG, "[renderer] " + message);
 }
